a806eb5d35ef3d41019cf74a5bfb32dd3bee3273
KeithLamb <keithianlamb@gmail.com>
2024-05-01 22:13:23 -0500

    Finished Changing states of code with react

--- frontend/components/Counter.js
+++ frontend/components/Counter.js
@@ -45,20 +45,25 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
-
+  const [count , setCount] = useState(0)
   const increment = () => {
     /* STEP 4 */
+    setCount(count + 1)
   };
   const decrement = () => {
     /* STEP 5 */
+    setCount(count - 1)
   };
   const reset = () => {
     /* STEP 6 */
+    setCount(0)
   };
+
+  const isEven = count % 2 === 0;
 
   const style = {
     fontSize: '1.5em',
@@ -70,7 +75,7 @@ export default function Counter() {
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {isEven ? 'even' : 'odd'} {/*step2*/}
       </div>
       <div>
         <button id='increment' onClick={increment}>Increment</button>
